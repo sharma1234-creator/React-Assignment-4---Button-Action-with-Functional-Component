204008494aa7011224285e0bfa2a6d232bcddc95
sharma1234-creator <72900717+sharma1234-creator@users.noreply.github.com>
2021-02-23 22:36:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  
+  const [set,Csettime] = useState(0);
+  const handleClick = () =>{
+    Csettime("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
       // Do not alter the main div
+      <p id="para">{set}</p>
+      <button id="click" onClick={handleClick} >open</button>
     </div>
   );
 }
